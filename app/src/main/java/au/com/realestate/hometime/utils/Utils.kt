a3f9167ecdad94d58317a17da4ae33aae910498d
Sharad <shrivastava.lucky@gmail.com>
2019-05-29 17:19:08 +1000

    Unit testing done.

--- app/src/main/java/au/com/realestate/hometime/utils/Utils.kt
+++ app/src/main/java/au/com/realestate/hometime/utils/Utils.kt
@@ -1,11 +1,7 @@
 package au.com.realestate.hometime.utils
 
 import android.content.Context
-import au.com.realestate.hometime.R
-import au.com.realestate.hometime.TramApp
-import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 object Utils {
 
@@ -14,10 +10,6 @@ object Utils {
         val endIndex = dotNetDate.indexOf("+")
         val date = dotNetDate.substring(startIndex, endIndex)
         return Date(date.toLong())
-    }
-
-    fun dpFromPx(context: Context, px: Float): Float {
-        return px / context.resources.displayMetrics.density
     }
 
     fun pxFromDp(context: Context?, dp: Float): Float {
