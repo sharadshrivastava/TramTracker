9189b11ace435d14d57a13a43ee2142a80dae902
Sharad <shrivastava.lucky@gmail.com>
2019-05-30 18:02:46 +1000

    Testing code Changed to Kotlin code

---
+++ app/src/test/java/au/com/realestate/hometime/UtilsTest.kt
@@ -0,0 +1,51 @@
+package au.com.realestate.hometime
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
+import au.com.realestate.hometime.utils.Utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+
+@RunWith(MockitoJUnitRunner::class)
+class UtilsTest {
+
+    @Mock
+    var mContext: Context? = null
+    @Mock
+    var mResources: Resources? = null
+    @Mock
+    var mDisplayMetrics: DisplayMetrics? = null
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this)
+
+        Mockito.`when`(mContext?.resources).thenReturn(mResources)
+        Mockito.`when`(mResources?.displayMetrics).thenReturn(mDisplayMetrics)
+        mDisplayMetrics?.density = 5f
+    }
+
+    @Test
+    fun testDateFromDotNetDate() {
+        val expected = "Wed May 29 14:29:21 AEST 2019"
+        val date = Utils.dateFromDotNetDate("/Date(1559104161873+1000)/")
+        assertEquals(expected, date.toString())
+    }
+
+    @Test
+    fun testPxFromDp() {
+        val expected = 80f
+        val actual = Utils.pxFromDp(mContext, 16f)
+        assertTrue(expected == actual)
+    }
+}
